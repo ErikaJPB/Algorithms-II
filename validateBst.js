beb0e4795f1000ea3832c6aabc63f2f762f16f59
Erika Pineda <109609521+ErikaJPB@users.noreply.github.com>
2023-02-11 10:13:57 -0500

    Add files via upload

---
+++ validateBst.js
@@ -0,0 +1,31 @@
+//                                10
+//                               /  \
+//                             5     15
+//                           /   \   / \
+//                          2    5  13  22
+//                        /          \
+//                       1           14
+
+// output: true
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function validateBst(tree) {
+  // Write your code here.
+  return helper(tree, -Infinity, Infinity);
+}
+
+function helper(tree, minValue, maxValue) {
+  if (!tree) return true;
+  if (tree.value < minValue || tree.value >= maxValue) return false;
+  return (
+    helper(tree.left, minValue, tree.value) &&
+    helper(tree.right, tree.value, maxValue)
+  );
+}
