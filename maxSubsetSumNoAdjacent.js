a1c02c2bad7e09ecf1a8acc94ce133e49e99a99c
Erika Pineda <109609521+ErikaJPB@users.noreply.github.com>
2023-02-21 10:07:04 -0500

    Add files via upload

---
+++ maxSubsetSumNoAdjacent.js
@@ -0,0 +1,20 @@
+// O(n) time | O(1) space
+
+function maxSubsetSumNoAdjacent(array) {
+  // Write your code here.
+
+  if (!array.length) return 0;
+  if (array.length === 1) return array[0];
+
+  let second = array[0];
+  let first = Math.max(array[0], array[1]);
+
+  for (let i = 2; i < array.length; i++) {
+    let current = Math.max(first, second + array[i]);
+    second = first;
+    first = current;
+  }
+  return first;
+}
+
+console.log(maxSubsetSumNoAdjacent([75, 105, 120, 75, 90, 135]));
